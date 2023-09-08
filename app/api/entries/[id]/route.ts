0338d10a8d210838bd99fb2bb9e915637ccff503
Rances Lacruz <rances@Rancess-MacBook-Pro-6.local>
2023-09-08 17:43:32 -0300

    chore: add put enpoint

---
+++ app/api/entries/[id]/route.ts
@@ -0,0 +1,43 @@
+import { db } from '@/database'
+import { Entry } from '@/models'
+import mongoose from 'mongoose'
+import { NextResponse } from 'next/server'
+
+export async function PUT(req: Request) {
+  const id = req.url.split('entries/')[1]
+
+  if (!mongoose.isValidObjectId(id)) {
+    return NextResponse.json({ message: 'bad request' }, { status: 400 })
+  }
+
+  await db.connect()
+  const entryToUpdate = await Entry.findById(id)
+
+  if (!entryToUpdate) {
+    await db.disconnect()
+    return NextResponse.json({ message: 'id not found' }, { status: 400 })
+  }
+  const {
+    description = entryToUpdate?.description,
+    status = entryToUpdate?.status,
+  } = await req.json()
+  try {
+    const updatedEntry = await Entry.findByIdAndUpdate(
+      id,
+      {
+        description,
+        status,
+      },
+      { runValidators: true, new: true }
+    )
+    await db.disconnect()
+    return NextResponse.json({ updatedEntry }, { status: 200 })
+  } catch (error: any) {
+    console.log({ error })
+    await db.disconnect()
+    return NextResponse.json(
+      { message: error.errors.status.message.message },
+      { status: 400 }
+    )
+  }
+}
